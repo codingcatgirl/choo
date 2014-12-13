bdf09f83b6ba508ad5c1978510d7246173e86a75
Laura Klünder <laura@codingcatgirl.de>
2014-12-13 20:48:33 +0100

    Better serialization

--- test.py
+++ test.py
@@ -8,4 +8,5 @@ vrr = VRR()
 du = Stop(city='essen', name='borbeck süd bf')
 
 result = vrr.get_stop_rides(du)
+test = Stop.unserialize(result.serialize())
 print(json.dumps(result.serialize(), indent=2))
