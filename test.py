1514f66d4e923373f1dd136b332bb1480dfd9bfa
Nils Martin Klünder <webmaster@nomoketo.de>
2015-04-22 21:51:32 +0200

    trip requests

--- test.py
+++ test.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python3
 from networks import VRR
-from models import Stop
+from models import Stop, Trip
 from datetime import datetime
 from output import PrettyPrint
 import json
 
 vrr = VRR()
 du = Stop(city='essen', name='hbf')
-du = Stop(city='essen', name='borbeck süd bf')
+bs = Stop(city='essen', name='borbeck süd bf')
 
-result = vrr.get_stop_rides(du)
-p = PrettyPrint()
-print(p.formatted(result))
+trip = Trip()
+trip.origin = du
+trip.destination = bs
+
+vrr.get_trips()
+#result = vrr.get_stop_rides(du)
+#p = PrettyPrint()
+#print(p.formatted(result))
 # print(json.dumps(result.serialize(), indent=2))
