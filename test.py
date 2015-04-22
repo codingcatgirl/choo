050de27c62add488bf935354c3dd730a1f07a18b
Nils Martin Klünder <webmaster@nomoketo.de>
2015-04-22 16:33:31 +0200

    fix typo

--- test.py
+++ test.py
@@ -6,11 +6,8 @@ from output import PrettyPrint
 import json
 
 vrr = VRR()
-<<<<<<< HEAD
 du = Stop(city='essen', name='hbf')
-=======
 du = Stop(city='essen', name='borbeck süd bf')
->>>>>>> 2b6f041581cb63bbf168ee9f5d5482a7ae93e245
 
 result = vrr.get_stop_rides(du)
 p = PrettyPrint()
