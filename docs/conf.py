ed04e53142769e5a0dc204a56d6b19b24886ef6d
Nils Martin Klünder <nomoketo@nomoketo.de>
2015-04-25 20:30:03 +0200

    2015

--- docs/conf.py
+++ docs/conf.py
@@ -45,7 +45,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'transit'
-copyright = '2014, Nils Martin Klünder'
+copyright = '2015, Nils Martin Klünder'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
