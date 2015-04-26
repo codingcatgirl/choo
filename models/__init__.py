6967f3f3c056879c6d306e17a21056631ee6782a
Laura Klünder <laura@codingcatgirl.de>
2015-04-26 15:16:39 +0200

    import Coordinates

--- models/__init__.py
+++ models/__init__.py
@@ -3,5 +3,6 @@ from .location import Coordinates, Location, Address, POI, Way
 from .linetypes import LineType, LineTypes
 from .realtime import RealtimeTime
 from .main import Trip, Ride, TimeAndPlace, Line, Stop
-__all__ = ['Location', 'Stop', 'Address', 'POI', 'Line', 'LineType',
-           'LineTypes', 'RealtimeTime', 'TimeAndPlace', 'Ride', 'Trip', 'Way']
+__all__ = ['Coordinates', 'Location', 'Stop', 'Address', 'POI', 'Line',
+           'LineType', 'LineTypes', 'RealtimeTime', 'TimeAndPlace', 'Ride',
+           'Trip', 'Way']
