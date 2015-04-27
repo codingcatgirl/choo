f896637988da432f096485dbce716994776a5469
Nils Martin Klünder <webmaster@nomoketo.de>
2015-04-27 07:30:32 +0200

    Model Serialization

--- models/__init__.py
+++ models/__init__.py
@@ -3,6 +3,10 @@ from .location import Coordinates, Location, Address, POI, Way
 from .linetypes import LineType, LineTypes
 from .realtime import RealtimeTime
 from .main import Trip, Ride, TimeAndPlace, Line, Stop
+
+def unserialize_typed():
+    return globals()[data[0]].unserialize(data[1])
+
 __all__ = ['Coordinates', 'Location', 'Stop', 'Address', 'POI', 'Line',
            'LineType', 'LineTypes', 'RealtimeTime', 'TimeAndPlace', 'Ride',
-           'Trip', 'Way']
+           'Trip', 'Way', 'unserialize_typed']
