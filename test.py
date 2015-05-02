bf9c1e3f9d02f7af265d9430a8c12b8e7d9ea2cc
Laura Klünder <laura@codingcatgirl.de>
2015-05-02 16:46:48 +0200

    use get( not is None stuff

--- test.py
+++ test.py
@@ -13,8 +13,8 @@ trip = Trip.Request()
 trip.origin = bs
 trip.destination = du
 
-#result = vrr.search_trips(trip)
-result = vrr.get_stop_rides(bs)
+result = vrr.search_trips(trip)
+#result = vrr.get_stop_rides(bs)
 #p = PrettyPrint()
 #print(p.formatted(result))
 print(json.dumps(result.serialize(typed=True), indent=2))
