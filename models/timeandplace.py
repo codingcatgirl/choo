fec5a261e009bdeeb49dd6104724c1ddd3676af7
Laura Klünder <laura@codingcatgirl.de>
2015-05-06 16:28:39 +0200

    introducing the platform model

--- models/timeandplace.py
+++ models/timeandplace.py
@@ -4,6 +4,28 @@ from .locations import Coordinates, Stop
 from .realtime import RealtimeTime
 
 
+class Platform(ModelBase):
+    def __init__(self, stop=None, name=None):
+        super().__init__()
+        self.stop = stop
+        self.name = name
+
+    @classmethod
+    def _validate(cls):
+        return {
+            'stop': (None, Stop),
+            'name': str
+        }
+
+    def _serialize(self, depth):
+        data = {}
+        data['name'] = self.name
+        return data
+
+    def _unserialize(self, data):
+        self.name = data['name']
+
+
 class TimeAndPlace(ModelBase):
     def __init__(self, stop=None, platform=None, arrival=None, departure=None, coords=None):
         super().__init__()
@@ -16,8 +38,7 @@ class TimeAndPlace(ModelBase):
     @classmethod
     def _validate(cls):
         return {
-            'stop': Stop,
-            'platform': (None, str),
+            'platform': (None, Platform),
             'coords': (None, Coordinates),
             'arrival': (None, RealtimeTime),
             'departure': (None, RealtimeTime)
@@ -25,8 +46,9 @@ class TimeAndPlace(ModelBase):
 
     def _serialize(self, depth):
         data = {}
-        self._serial_add(data, 'platform')
         data['stop'] = self.stop.serialize(depth)
+        if self.platform:
+            data['platform'] = self.platform.serialize()
         if self.coords:
             data['coords'] = self.coords.serialize()
         if self.arrival:
