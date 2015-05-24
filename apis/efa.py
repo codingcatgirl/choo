540f302a6f5c6e8dd13f5637e96f2e70e57b7120
Nils Martin Klünder <webmaster@nomoketo.de>
2015-05-24 18:30:52 +0200

    train station name guessing replaced some letters in the middle of the word

--- apis/efa.py
+++ apis/efa.py
@@ -319,9 +319,10 @@ class EFA(API):
             else:
                 return
 
-        name = stop.name
+        name = stop.name+' '
         for suffix, replacement in self.train_station_suffixes.items():
             name = name.replace(suffix, replacement)
+        name = name[:-1]
         stop.train_station_name = ('%s %s' % ('' if stop.city is None or name.startswith(stop.city) else stop.city, name)).strip()
 
     def _parse_stop_line(self, data):
