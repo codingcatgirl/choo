a4e7d413bff695190a108dcd426420a3a861f402
Laura Klünder <laura@codingcatgirl.de>
2015-06-17 15:21:39 +0200

    msgpack support for transit-cli and several improvements

--- cli.py
+++ cli.py
@@ -9,43 +9,67 @@ import argparse
 supported = networks.supported
 
 parser = argparse.ArgumentParser()
-parser.add_argument('network', type=str, choices=('networks', ) + networks.supported)
-parser.add_argument('search', nargs='?', type=str)
-parser.add_argument('--pretty', action='store_true')
+parser.add_argument('network', type=str, choices=('networks', ) + networks.supported, help='network/API to use. “networks” lists all available networks')
+parser.add_argument('query', nargs='?', type=str)
+parser.add_argument('--input', type=str, choices=('json', 'msgpack'), default='json', help='input format')
+parser.add_argument('--output', type=str, choices=('json', 'prettyjson', 'msgpack'), default='json', help='output format')
 
 args = parser.parse_args()
 
 if args.network == 'networks':
-    print(json.dumps(networks.supported))
-    sys.exit(0)
-
-network = networks.network(args.network)()
-
-if args.search is None:
-    # Interactive Session
-    print('Coming Soon')
-    sys.exit(0)
-
-if args.search == '-':
-    search = sys.stdin.read()
+    result = networks.supported
 else:
-    f = open(args.search)
-    search = f.read()
-    f.close()
+    network = networks.network(args.network)()
 
-try:
-    search = unserialize_typed(json.loads(search))
-except:
-    traceback.print_exc()
-    sys.exit(3)
+    if args.query is None:
+        # Interactive Session
+        print('Coming Soon')
+        sys.exit(0)
 
-try:
-    result = network.query(search)
-except NotImplementedError:
-    print('This network does not support this action.')
-    sys.exit(4)
+    if args.input == 'json':
+        if args.query == '-':
+            query = sys.stdin.read()
+        else:
+            f = open(args.query)
+            query = f.read()
+            f.close()
 
-if args.pretty:
-    print(json.dumps(result.serialize(), indent=2))
-else:
+        query = json.loads(query)
+
+    elif args.input == 'msgpack':
+        try:
+            import msgpack
+        except:
+            raise ImportError('Please install: pip install msgpack-python')
+
+        if args.query == '-':
+            query = sys.stdin.buffer.read()
+        else:
+            f = open(args.query, 'rb')
+            query = f.read()
+            f.close()
+
+        query = msgpack.unpackb(query)
+
+    try:
+        query = unserialize_typed(query)
+    except:
+        traceback.print_exc()
+        sys.exit(3)
+
+    try:
+        result = network.query(query)
+    except NotImplementedError:
+        print('This network does not support this action.')
+        sys.exit(4)
+
+if args.output == 'json':
     print(json.dumps(result.serialize(), separators=(',', ':')))
+elif args.output == 'prettyjson':
+    print(json.dumps(result.serialize(), indent=2))
+elif args.output == 'mspack':
+    try:
+        import msgpack
+    except:
+        raise ImportError('Please install: pip install msgpack-python')
+    sys.stdout.buffer.write(msgpack.packb(result.serialize(), use_bin_type=True))
