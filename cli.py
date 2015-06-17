9dd1e37e06530d8011ffc128d37f55fd84b31cc9
Nils Martin Klünder <webmaster@nomoketo.de>
2015-06-17 12:20:34 +0200

    New cli API

--- cli.py
+++ cli.py
@@ -1,80 +1,51 @@
 #!/usr/bin/env python3
-from models import ModelBase
-from output import PrettyPrint
+from models import Serializable, unserialize_typed
 import sys
 import networks
 import json
 import traceback
+import argparse
 
-argv = sys.argv[1:]
-argc = len(argv)
+supported = networks.supported
 
-methods = {
-    'get_stop': ('Stop', ),
-    'get_stop_rides': ('Stop', )
-}
+parser = argparse.ArgumentParser()
+parser.add_argument('network', type=str, choices=('networks', ) + networks.supported)
+parser.add_argument('search', nargs='?', type=str)
+parser.add_argument('--pretty', action='store_true')
 
-if argc == 1 and argv[0] == 'networks':
+args = parser.parse_args()
+
+if args.network == 'networks':
     print(json.dumps(networks.supported))
     sys.exit(0)
-elif argc == 1 and argv[0] == 'methods':
-    print(json.dumps(methods))
-elif argc > 1:
-    if argv[0] not in networks.supported:
-        print('Unknown network.')
-        sys.exit(2)
-    network = networks.network(argv[0])()
-    if argv[1] not in methods:
-        print('Unknown method.')
-        sys.exit(2)
-    method = argv[1]
-    argv = argv[2:]
 
-    myformat = 'nojson'
-    if argv[0] in ('--nojson', '--json', '--json-noraws'):
-        myformat = argv[0][2:]
-        argv = argv[1:]
+network = networks.network(args.network)()
 
-    maxc = len(methods[method])
-    minc = len([item for item in methods[method] if type(item) == str or None not in item])
-    if len(argv) > maxc or len(argv) < minc:
-        print('Invalid Argument count for this method.')
-        sys.exit(2)
+if args.search is None:
+    # Interactive Session
+    print('Coming Soon')
+    sys.exit(0)
 
-    args = []
-    for arg in argv:
-        if arg == '-':
-            data = ''
-            for line in sys.stdin:
-                data += line
-        else:
-            f = open(arg)
-            data = f.read()
-            f.close()
-        args.append(data)
-
-    try:
-        args = [ModelBase.unserialize(json.loads(item)) for item in args]
-    except:
-        traceback.print_exc()
-        sys.exit(3)
-
-    try:
-        result = getattr(network, method)(*args)
-    except NotImplementedError:
-        print('This method is not supported by this network.')
-        sys.exit(4)
-
-    if myformat == 'nojson':
-        p = PrettyPrint()
-        print(p.formatted(result))
-    elif myformat == 'json-noraws':
-        print(json.dumps(result.serialize(['noraws'])))
-    else:
-        print(json.dumps(result.serialize()))
+if args.search == '-':
+    search = sys.stdin.read()
 else:
-    print('  transit-cli networks')
-    print('  transit-cli methods')
-    print('  transit-cli <network> <method> [ --pretty | --nopretty ] <arguments>')
-    print('Invalid Syntax.')
-    sys.exit(2)
+    f = open(args.search)
+    search = f.read()
+    f.close()
+
+try:
+    search = unserialize_typed(json.loads(search))
+except:
+    traceback.print_exc()
+    sys.exit(3)
+
+try:
+    result = network.query(search)
+except NotImplementedError:
+    print('This network does not support this action.')
+    sys.exit(4)
+
+if args.pretty:
+    print(json.dumps(result.serialize(), indent=2))
+else:
+    print(json.dumps(result.serialize(), separators=(',', ':')))
