bb0c9465026999aeff0c7c233f1a7db1e67bb29b
Laura Klünder <laura@codingcatgirl.de>
2015-06-26 20:43:21 +0200

    improve Collection performance

--- models/collection.py
+++ models/collection.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 from .base import Serializable, Collectable
-
+import time
 
 class Collection(Serializable):
     def __init__(self):
         self.known = {}
         self.by_id = {}
+        self.by_pid = {}
 
     def _serialize(self):
         return {model: [obj.serialize() for obj in objects] for model, objects in self.known.items()}
@@ -25,27 +26,43 @@ class Collection(Serializable):
         if not by_id:
             return
 
+        if id(obj) in self.by_pid:
+            return self.by_pid[id(obj)]
+
         for name, value in obj._ids.items():
             found = by_id.get(name, {}).get(value)
             if found is not None:
-                if model == 'Platform' and found.stop != obj.stop:
-                    continue
                 if model == 'Ride' and found != obj:
                     continue
                 return found
 
+        if model == 'Platform' and obj.name is None and obj.full_name is None:
+            return
+
         for item in self.known.get(model):
-            if item == obj:
-                return item
+            for name in obj._ids:
+                if name in item._ids:
+                    break
+            else:
+                if item == obj:
+                    return item
 
     def add(self, obj):
+        model = obj._serialized_name()
+        if model == 'Platform' and obj.name is None and obj.full_name is None:
+            return obj
+
         found = self.retrieve(obj)
+        is_new = False
         if found is not None:
+            if found is obj:
+                return found
             found.update(obj)
         else:
             found = obj
+            self.by_pid[id(found)] = found
+            is_new = True
 
-        model = obj._serialized_name()
         ids = {name: value for name, value in obj._ids.items() if type(value) != tuple or None not in value}
         if model not in self.by_id:
             self.by_id[model] = {name: {value: found} for name, value in ids.items()}
@@ -57,6 +74,7 @@ class Collection(Serializable):
                     by_id[name] = {}
                 by_id[name][value] = found
 
+        if is_new:
             self.known[model].append(found)
 
         return found
