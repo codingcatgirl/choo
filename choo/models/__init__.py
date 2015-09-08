fbb5ccbe141254704d84481dc0cbd5b433979e74
Laura Klünder <laura@codingcatgirl.de>
2015-09-08 16:42:54 +0200

    goodbye Collection

--- choo/models/__init__.py
+++ choo/models/__init__.py
@@ -8,11 +8,10 @@ from .ride import Ride, RideSegment
 from .way import Way, WayType, WayEvent
 from .timeandplace import TimeAndPlace
 from .realtime import RealtimeTime
-from .collection import Collection
 
 
 __all__ = ['Serializable', 'Searchable', 'Collectable',
-           'Collection', 'TripPart', 'Coordinates', 'AbstractLocation',
+           'TripPart', 'Coordinates', 'AbstractLocation',
            'Platform', 'Location', 'Stop', 'Address', 'POI', 'Line', 'LineType',
            'LineTypes', 'RealtimeTime', 'TimeAndPlace', 'Platform', 'Ride',
            'RideSegment', 'Trip', 'Way', 'WayType', 'WayEvent', 'TicketList',
