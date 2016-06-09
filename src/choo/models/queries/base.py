5b9255f53e8599faf74648d244d8ee2d4eaefc05
Laura Klünder <laura@codingcatgirl.de>
2016-06-09 14:49:35 +0200

    make Query compatible to generators

--- src/choo/models/queries/base.py
+++ src/choo/models/queries/base.py
@@ -1,5 +1,6 @@
 from ..base import Field, Model
 from collections import ChainMap, OrderedDict
+from itertools import chain
 
 
 class MetaQuery(type):
@@ -30,8 +31,11 @@ class Query(metaclass=MetaQuery):
     def __init__(self):
         if self.__class__ == Query:
             raise TypeError('only subclasses of Query can be initialised')
+
         self._data = self._settings.copy()
-        self._result = None
+        self._cached_results = None
+        self._results_generator = None
+        self._results_done = False
 
     def update(self, **kwargs):
         result = self.__class__()
@@ -58,13 +62,27 @@ class Query(metaclass=MetaQuery):
             raise self.Model.NotFound
         return r[0]
 
-    def execute(self):
-        if self._result is None:
-            self._result = self._execute()
-        return self._result
+    def _execute(self, obj):
+        raise TypeError('Cannot execute query not bound to a network')
 
-    def iter(self):
-        iter(self.execute())
+    def execute(self):
+        if self._results_generator is None:
+            self._results_generator = self._execute()
+        return self
+
+    def __iter__(self):
+        self.execute()
+
+        if self._results_done:
+            return iter(self._cached_results)
+
+        return chain(self._cached_results, self._next_result())
+
+    def _next_result(self):
+        for result in self._results_generator:
+            self._cached_results.append(result)
+            yield result
+        self._results_done = True
 
     def __getattr__(self, name):
         if name not in self._combined:
