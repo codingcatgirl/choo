75560ec7a7a36b8f1b763b4f7ccb8f94f51dec30
Laura Klünder <laura@codingcatgirl.de>
2016-06-14 21:13:26 +0200

    add LocationQuery.max_distance

--- src/choo/queries/locations.py
+++ src/choo/queries/locations.py
@@ -4,6 +4,13 @@ from .base import Query
 
 class LocationQuery(Query):
     Model = Location
+    _settings_defaults = {'max_distance': 1000}
+
+    def max_distance(self, max_distance):
+        if not isinstance(max_distance, int) or max_distance < 1:
+            raise TypeError('max_distance has to be int > 0')
+        self._update_setting('max_distance', max_distance)
+        return self
 
 
 class AddressQuery(Query):
