bc68cc441ded5440a200875645651f21187a5e30
Laura Klünder <laura@codingcatgirl.de>
2016-06-18 15:10:10 +0200

    remove Query from queries.__all__

--- src/choo/queries/__init__.py
+++ src/choo/queries/__init__.py
@@ -1,5 +1,5 @@
 from .base import Model
 from .locations import GeoPointQuery, PlatformQuery, AddressQuery, AddressableQuery, LocationQuery, POIQuery, StopQuery
 
-__all__ = ['Model', 'Query', 'GeoPointQuery', 'PlatformQuery', 'AddressableQuery', 'AddressQuery',
+__all__ = ['Model', 'GeoPointQuery', 'PlatformQuery', 'AddressableQuery', 'AddressQuery',
            'LocationQuery', 'POIQuery', 'StopQuery']
