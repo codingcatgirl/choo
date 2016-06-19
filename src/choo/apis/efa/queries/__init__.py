a980b9e9e5ab17321362558e7d1f5c9f7567f58f
Laura Klünder <laura@codingcatgirl.de>
2016-06-19 14:40:11 +0200

    add EFA queries to __all__

--- src/choo/apis/efa/queries/__init__.py
+++ src/choo/apis/efa/queries/__init__.py
@@ -1,3 +1,4 @@
-from .locations import AddressQuery, LocationQuery, POIQuery, StopQuery
+from .locations import GeoPointQuery, PlatformQuery, AddressQuery, AddressableQuery, LocationQuery, POIQuery, StopQuery
 
-__all__ = ['AddressQuery', 'LocationQuery', 'POIQuery', 'StopQuery']
+__all__ = ['GeoPointQuery', 'PlatformQuery', 'LocationQuery', 'AddressQuery', 'AddressableQuery', 'POIQuery',
+           'StopQuery']
