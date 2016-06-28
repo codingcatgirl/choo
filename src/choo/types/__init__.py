7818231e5662c51cd8d7f81381ddf6ef406431d0
Laura Klünder <laura@codingcatgirl.de>
2016-06-28 17:13:17 +0200

    fix missing import in types.__init__

--- src/choo/types/__init__.py
+++ src/choo/types/__init__.py
@@ -1,8 +1,8 @@
-from .enums import WayType, WayEvent, LineType, POIType, PlatformType, LineTypes
+from .enums import WayType, WayEvent, WalkSpeed, LineType, POIType, PlatformType, LineTypes
 from .misc import Serializable, Coordinates, LiveTime
 from .ids import IDs, FrozenIDs, IFOPT, StopIFOPT, StopAreaIFOPT, PlatformIFOPT
 
 
-__all__ = ['WayType', 'WayEvent', 'LineType', 'POIType', 'PlatformType', 'LineTypes',
+__all__ = ['WayType', 'WayEvent', 'WalkSpeed', 'LineType', 'POIType', 'PlatformType', 'LineTypes',
            'Serializable', 'Coordinates', 'LiveTime',
            'IDs', 'FrozenIDs', 'IFOPT', 'StopIFOPT', 'StopAreaIFOPT', 'PlatformIFOPT']
