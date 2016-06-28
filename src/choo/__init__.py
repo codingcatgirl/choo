e2c7145cca25aebf29a0ded1b31578c941a0bf58
Laura Klünder <laura@codingcatgirl.de>
2016-06-28 15:00:22 +0200

    fix circular imports but dont break setup.py

--- src/choo/__init__.py
+++ src/choo/__init__.py
@@ -1,3 +1,6 @@
-from . import networks, models, queries, apis  # noqa
+try:
+    from . import networks  # prevent circular imports  # noqa
+except ImportError:
+    pass  # users will get the exception anyway when they try to import networks
 
 __version__ = '0.2.0'
